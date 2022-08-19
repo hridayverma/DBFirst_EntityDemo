41c669f59cd6827efb770f8cad0dfa18c478a117
hridayverma <hrid.06@gmail.com>
2022-08-19 13:12:02 +0530

    Add project files.

---
+++ DBFirst_EntityDemo/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DBFirst_EntityDemo
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ DBFirst_EntityDemo/Controllers/StateMgtController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DBFirst_EntityDemo.Controllers
+{
+    public class Employee
+    {       
+        public int Id { get; set; }
+    }
+    public class StateMgtController : Controller
+    {
+        // GET: StateMgt
+        public ActionResult Index()
+        {
+            //Adding data in ViewBag,ViewData and TempData
+            ViewBag.Employee = new Employee() { Id = 1000 };
+            ViewData["employee2"] = new Employee() { Id = 2000 };
+            TempData["Message"] = "Data from TempData";
+            TempData.Keep("Message");
+           // TempData.Peek()
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            //Retreiving Data from TempData on second Request
+            ViewBag.Message = TempData["Message"];
+            //TempData.Keep("Message");
+            return View();
+        }
+
+        public ActionResult Index3()
+        {
+            //Retreiving Data from TempData on Third Request
+            ViewBag.Message = TempData["Message"];
+            return View();
+        }
+    }
+}
